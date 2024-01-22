41b4817ff7db2a378fdac0568de0b8f0402d3b12
sudomaster00081 <ajayprasadd0008@gmail.com>
2024-01-22 19:28:46 +0530

    + Modified batch.bat + created copy.bat for merging all the Files, approx 30 GB + Added .csv to be ignored + Created Implimentation Folder

--- To Victim/to-victim/batch.bat
+++ To Victim/to-victim/batch.bat
@@ -1,18 +1,20 @@
 @echo off
+setlocal enabledelayedexpansion
 
 rem Process each .pcap file in the current directory
 for %%i in (*.pcap) do (
   set input_file=%%~fi
   set file_name=%%~ni
-  set output_file=%file_name%.csv
+  set output_file=!file_name!.csv
   
-  echo Processing: %file_name%
+  echo Processing: !file_name!
   
-  tshark -r "%input_file%" -T fields -E header=y -E separator=, -E quote=d -E occurrence=f ^
+  tshark -r "!input_file!" -T fields -E header=y -E separator=, -E quote=d -E occurrence=f ^
     -e ip.src -e ip.dst -e frame.protocols -e frame.len -e frame.time_delta_displayed ^
-    -e tcp.dstport -e udp.dstport -e ip.proto -e frame.time_relative > "%output_file%"
+    -e tcp.dstport -e udp.dstport -e ip.proto -e frame.time_relative > "!output_file!"
   
-  echo Done: %file_name%
+  echo Done: !file_name!
 )
 
 echo All files processed.
+endlocal
